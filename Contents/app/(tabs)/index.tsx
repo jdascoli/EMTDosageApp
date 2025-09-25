e8b38334a5c3a399efa55282938f5816fd332c69
AlexanderMiller21 <112978182+AlexanderMiller21@users.noreply.github.com>
2025-09-25 16:09:39 -0500

    Update index.tsx

--- Contents/app/(tabs)/index.tsx
+++ Contents/app/(tabs)/index.tsx
@@ -43,7 +43,7 @@ export default function HomeScreen() {
         Medications
       </ThemedText>
       <FlatList
-        data={medications}
+        data={alphabeticalList()}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         contentContainerStyle={styles.list}
