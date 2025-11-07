b0d1795e067a97f1748eda9e2f6fa6e0ce28483c
AlexanderMiller21 <112978182+AlexanderMiller21@users.noreply.github.com>
2025-11-07 11:03:09 -0600

    Testable Certification Filter
    Added a column to medications called minCert. Which uses an integer to store the id of a certification level and compare it against user Certification level.

--- Contents/app/(tabs)/_layout.tsx
+++ Contents/app/(tabs)/_layout.tsx
@@ -21,27 +21,32 @@ export default function TabLayout() {
       await upsertMedication(
         "Epinephrine",
         "Increases heart rate, blood pressure, and dilates airways (used in anaphylaxis).",
-        "Epinephrine can induce cardiac arrhythmias, chest pain, and myocardial ischemia, especially in patients with coronary artery disease, hypertension, or other organic heart diseases. It may also cause rapid increases in blood pressure that can lead to cerebral hemorrhage."
+        "Epinephrine can induce cardiac arrhythmias, chest pain, and myocardial ischemia, especially in patients with coronary artery disease, hypertension, or other organic heart diseases. It may also cause rapid increases in blood pressure that can lead to cerebral hemorrhage.",
+        1
       );
       await upsertMedication(
         "Aspirin",
         "Reduces pain, fever, and inflammation; inhibits platelet aggregation.",
-        "Patients can be allergic to Aspirin. Those with asthma or known bronchospasm associated with NSAIDs should be cautious. Increases risk of GI bleeding in patients with peptic ulcer disease or gastritis."
+        "Patients can be allergic to Aspirin. Those with asthma or known bronchospasm associated with NSAIDs should be cautious. Increases risk of GI bleeding in patients with peptic ulcer disease or gastritis.",
+        1
       );
       await upsertMedication(
         "Nitroglycerin",
         "Relaxes blood vessels, improving blood flow (used in angina).",
-        "Contraindicated in patients with severe anemia, right-sided myocardial infarction, or those using PDE-5 inhibitors (e.g., sildenafil, tadalafil). Combining the two can cause dangerous hypotension."
+        "Contraindicated in patients with severe anemia, right-sided myocardial infarction, or those using PDE-5 inhibitors (e.g., sildenafil, tadalafil). Combining the two can cause dangerous hypotension.",
+        3
       );
       await upsertMedication(
         "Albuterol",
         "Relaxes airway muscles to improve breathing (used in asthma/COPD).",
-        "Avoid use in patients with known hypersensitivity to albuterol or its components; caution in patients with cardiac disorders due to possible tachycardia and hypertension."
+        "Avoid use in patients with known hypersensitivity to albuterol or its components; caution in patients with cardiac disorders due to possible tachycardia and hypertension.",
+        3
       );
       await upsertMedication(
         "Naloxone",
         "Blocks opioid receptors to reverse overdoses.",
-        "There are no absolute contraindications to naloxone use in emergencies. The only relative contraindication is known hypersensitivity to naloxone."
+        "There are no absolute contraindications to naloxone use in emergencies. The only relative contraindication is known hypersensitivity to naloxone.",
+        3
       );
 
       await upsertDosage("Epinephrine", 0.01, "mg", 0.3, null, 0, "Standard");
